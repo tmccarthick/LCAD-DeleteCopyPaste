3f31e647c66afce8fa79392c98a71b33e543f410
tmccarthick <tiffanyrlambert@gmail.com>
2017-10-06 14:19:17 -0500

    Initial repository setup. Adding .py script file  Initial commit

---
+++ DeleteCopyPaste.py
@@ -0,0 +1,4 @@
+Python 2.7.10 (default, May 23 2015, 09:40:32) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> #Delete, copy, paste for SA@LCAD filegeodatabase
+>>> 
